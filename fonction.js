bedcb2f1a295d18c1a39720f0df176dd210e4dc9
Jeiiro <dasilvatjeremy@gmail.com>
2022-05-29 21:38:47 +0200

    Première save avec JS

---
+++ fonction.js
@@ -0,0 +1,17 @@
+console.log("test")
+let container = 0;
+const button7 = 7;
+const button8 = 8;
+const button9 = 9;
+const button4 = 4;
+const button5 = 5;
+const button6 = 6;
+const button1 = 1;
+const button2 = 2;
+const button3 = 3;
+const button0 = 0;
+const buttonp = '.';
+let addition = function addition() {
+    return button1 + button2;
+}
+console.log(addition())
