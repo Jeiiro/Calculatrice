db50a42263c68f5e57a4af5df77078a68cc8c5d9
Jeiiro <dasilvatjeremy@gmail.com>
2022-06-20 22:07:08 +0200

    JS avec data-key

--- fonction.js
+++ fonction.js
@@ -1,17 +1,13 @@
-console.log("test")
-let container = 0;
-const button7 = 7;
-const button8 = 8;
-const button9 = 9;
-const button4 = 4;
-const button5 = 5;
-const button6 = 6;
-const button1 = 1;
-const button2 = 2;
-const button3 = 3;
-const button0 = 0;
-const buttonp = '.';
-let addition = function addition() {
-    return button1 + button2;
-}
-console.log(addition())
+const touches = [...document.querySelectorAll(".button")];
+const listeKeycode = touches.map((touche) => touche.dataset.key);
+document.addEventListener("keydown", (e) => {
+  const valeur = e.keyCode;
+});
+document.addEventListener("click", (e) => {
+  const valeur = e.target.dataset.key;
+});
+const calculer = (valeur) => {
+  if (listeKeycode.includes(valeur)) {
+    console.log(listeKeycode);
+  }
+};
